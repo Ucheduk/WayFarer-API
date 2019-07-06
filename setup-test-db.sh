aad1af9babf62792c9a8e50b7b725f90f0c3771b
Ucheduk <dukweb4@gmail.com>
2019-07-06 14:54:29 +0100

    Made setup-test-db.sh executable

--- setup-test-db.sh
+++ setup-test-db.sh
@@ -1,2 +1,2 @@
-psql wayfarer_test_db < migrations/schema.up.sql
-psql wayfarer_test_db < migrations/seed.sql
+psql wayfarer_test_db < ./migrations/schema.up.sql
+psql wayfarer_test_db < ./migrations/seed.sql
