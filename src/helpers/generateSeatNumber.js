fdd3b24792cf8f82d55165d12409519fc329008f
Ucheduk <dukweb4@gmail.com>
2019-07-14 03:30:59 +0100

    feature(refactor):refactor booking middleware[Finishes #167257045]

--- src/helpers/generateSeatNumber.js
+++ src/helpers/generateSeatNumber.js
@@ -1,7 +1,8 @@
 export default (list, capacity) => {
+  const bookedSeatList = list.map(i => i.seat_number);
   const newList = [];
   for (let i = 1; i <= capacity; i += 1) {
-    if (!list.includes(i)) {
+    if (!bookedSeatList.includes(i)) {
       newList.push(i);
     }
   }
